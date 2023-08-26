f7d7141a35fe121beb8b594624a968ceb5536bff
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-26 10:32:05 -0500

    Add route to update the user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import { createUser } from "../controllers/user.controller";
 import { createProduct } from "../controllers/product.controller";
 import { createProductPurchase } from "../controllers/productPurchase.controller";
 import { getUserWithPurchases } from "../controllers/getUser.controller";
+import { updateUserWithPurchases } from "../controllers/updateUser.controller";
 
 const router = express.Router();
 
 // Rutas de Usuarios
 router.post("/users", createUser);
 router.get("/users/:userId", getUserWithPurchases);
+router.put("/users/:userId", updateUserWithPurchases);
 
 // Rutas de Productos
 router.post("/products", createProduct);
