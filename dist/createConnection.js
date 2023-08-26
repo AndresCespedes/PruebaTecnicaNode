e393cccc5f0c2554f8a28f346bc37cdf310ea68a
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-25 19:48:27 -0500

    Delete unusless files

--- dist/createConnection.js
+++
@@ -1,9 +0,0 @@
-import { createConnection } from "typeorm";
-createConnection()
-    .then((connection) => {
-    console.log("Connected to database");
-})
-    .catch((error) => {
-    console.log("Database connection failed");
-    console.error(error);
-});
