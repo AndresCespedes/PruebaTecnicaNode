4d27d39e0c3d3b80a9456ef2c0ee5925ccaa90ff
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-25 20:33:50 -0500

    Add enpoint to get the user and relations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { createUser } from "../controllers/user.controller";
 import { createProduct } from "../controllers/product.controller";
 import { createProductPurchase } from "../controllers/productPurchase.controller";
+import { getUserWithPurchases } from "../controllers/getUser.controller";
 
 const router = express.Router();
 
 // Rutas de Usuarios
 router.post("/users", createUser);
+router.get("/users/:userId", getUserWithPurchases);
 
 // Rutas de Productos
 router.post("/products", createProduct);
