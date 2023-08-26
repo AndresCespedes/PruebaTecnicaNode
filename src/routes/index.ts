747f1a5b0c8577c11b19c03a0bf9f4e523c33b80
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-26 13:48:46 -0500

    add new routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller";
-import { createProduct } from "../controllers/product.controller";
-import { createProductPurchase } from "../controllers/productPurchase.controller";
-import { getUserWithPurchases } from "../controllers/getUser.controller";
-import { updateUserWithPurchases } from "../controllers/updateUser.controller";
+// Imports de usuarios
+import { createUser } from "../controllers/user/user.controller";
+import { getUserWithPurchases } from "../controllers/user/getUser.controller";
+import { updateUserWithPurchases } from "../controllers/user/updateUser.controller";
+import { deleteUserWithPurchases } from "../controllers/user/deleteUser.controller";
+//Imports de productos
+import { createProduct } from "../controllers/product/product.controller";
+import { getAllProducts } from "../controllers/product/getProduct.controller";
+import { updateProduct } from "../controllers/product/updateProduct.controller";
+import { deleteProduct } from "../controllers/product/deleteProduct.controller";
+
+//Imports de productos comprados
+import { createProductPurchase } from "../controllers/product/productPurchase.controller";
 
 const router = express.Router();
 
@@ -11,9 +19,13 @@ const router = express.Router();
 router.post("/users", createUser);
 router.get("/users/:userId", getUserWithPurchases);
 router.put("/users/:userId", updateUserWithPurchases);
+router.delete("/users/:userId", deleteUserWithPurchases);
 
 // Rutas de Productos
 router.post("/products", createProduct);
+router.get("/products/all-products", getAllProducts);
+router.put("/products/:productId", updateProduct);
+router.delete("/products/:productId", deleteProduct);
 
 // Rutas de Compras de Productos
 router.post("/product-purchases", createProductPurchase);
