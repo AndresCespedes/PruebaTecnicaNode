a10fdb2b4c8b760cad58186d88e05523fce2e11f
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-26 20:22:57 -0500

    Add the route for the OpenApi documentation

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/index";
 
+//Imports para realizar la documentación
+import swaggerUi from "swagger-ui-express";
+import { openApiConfig } from "../openapi.config";
+
 dotenv.config();
 
 const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openApiConfig));
 
 createConnection()
   .then(() => {
