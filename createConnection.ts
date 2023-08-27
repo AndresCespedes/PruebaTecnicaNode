cf541f498ea6693cb2cbfeee3f67bc878574ebd2
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-26 20:21:27 -0500

    Refactor de connection

--- createConnection.ts
+++ createConnection.ts
@@ -1,6 +1,15 @@
 import { createConnection } from "typeorm";
 
-createConnection()
+createConnection({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "Rufo1261!",
+  database: "products-users",
+  entities: [__dirname + "src/entities/*.ts"],
+  synchronize: true,
+})
   .then((connection) => {
     console.log("Connected to database");
   })
